d62d1b7f98e11b9137aac550ae12be275dc6ee59
Pg Gonni <p.gonni@hotmail.com>
2020-06-17 09:08:10 -0400

    Added TodoList component and style

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -1,22 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png" class="logo">
     <HelloWorld/>
+    <TodoList/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import TodoList from './components/TodoList'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TodoList,
   }
 }
 </script>
 
 <style>
+
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +38,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
+
 </style>
---
+++ todo-vue/src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done?">
+    TodoList goes here
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
